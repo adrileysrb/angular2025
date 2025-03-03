ff394c930f84a380b0107320a5481aea26cef089
ACT <ACT@NADF1717.bancoob.br>
2025-03-03 12:09:53 -0300

    bootstrap config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { sharedImports } from './shared.imports';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [...sharedImports, RouterOutlet, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
